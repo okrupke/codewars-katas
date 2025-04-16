cac48ce8885a348ebade8dcbede2f2fa1d7d006a
Oskar <oskar@Mac.fritz.box>
2025-04-16 23:04:22 +0200

    Sync Codewars solutions - 2025-04-16 23:04:22

---
+++ javascript/6-kyu/split_strings.js
@@ -0,0 +1,11 @@
+/**
+# Split Strings
+# split-strings
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+# Tags: Regular Expressions, Strings, Algorithms
+# Completed at: 2025-04-16
+
+*/
+
+// Solution goes here
