13cbaa309a57e19bf984f5a8f99cc96c716f6341
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-17 11:12:50 +0200

    Sync Codewars solutions - 2025-04-17 11:12:50

---
+++ javascript/7-kyu/mumbling.js
@@ -0,0 +1,11 @@
+/**
+# Mumbling
+# mumbling
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+# Tags: Fundamentals, Strings, Puzzles
+# Completed at: 2025-04-17
+
+*/
+
+// Solution goes here
