25408ee79557ea25ce6c18a63145fe1ce56ebb43
okrupke <156464481+okrupke@users.noreply.github.com>
2025-04-17 18:11:27 +0200

    Added code of shortest_word.js

--- javascript/7-kyu/shortest_word.js
+++ javascript/7-kyu/shortest_word.js
@@ -9,3 +9,27 @@
 */
 
 // Solution goes here
+// Function that finds the length of the shortest word in a string
+function findShort(s) {
+  // Split the input string into an array of words
+  let words = s.split(" ");
+
+  // Initialize result with a large number to ensure any word length will be smaller
+  let result = 11111111;
+
+  // Loop through each word in the array
+  for (word of words) {
+    // If the current word is shorter than the current result, update result
+    if (word.length < result) {
+      result = word.length;
+    }
+  }
+
+  // Return the length of the shortest word found
+  return result;
+}
+
+// Example usage and expected outputs:
+// findShort("The quick brown fox")       => 3 ("The")
+// findShort("bitcoin take over the world maybe who knows perhaps") => 3 ("the", "who")
+// findShort("Let's travel abroad shall we") => 2 ("we")
