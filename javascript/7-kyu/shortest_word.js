2ec62583a86dffe8cbebda341fad1f4a0e02ea48
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-17 18:09:56 +0200

    Sync Codewars solutions - 2025-04-17 18:09:56

---
+++ javascript/7-kyu/shortest_word.js
@@ -0,0 +1,11 @@
+/**
+# Shortest Word
+# shortest-word
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9
+# Tags: Fundamentals
+# Completed at: 2025-04-17
+
+*/
+
+// Solution goes here
