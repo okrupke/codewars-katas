a1fc36662cb86b68152f2fdc0c5fffbccf47a7e8
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-17 10:49:21 +0200

    Sync Codewars solutions - 2025-04-17 10:49:21

---
+++ javascript/6-kyu/detect_pangram.js
@@ -0,0 +1,11 @@
+/**
+# Detect Pangram
+# detect-pangram
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+# Tags: Strings, Data Structures, Fundamentals
+# Completed at: 2025-04-17
+
+*/
+
+// Solution goes here
