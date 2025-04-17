e79f0fc647f8f6ca71d39671c14bc62842d3e5b5
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-17 18:29:52 +0200

    Sync Codewars solutions - 2025-04-17 18:29:52

---
+++ javascript/6-kyu/duplicate_encoder.js
@@ -0,0 +1,11 @@
+/**
+# Duplicate Encoder
+# duplicate-encoder
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+# Tags: Strings, Arrays, Fundamentals
+# Completed at: 2025-04-17
+
+*/
+
+// Solution goes here
