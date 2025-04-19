ab96fb06f319d446b5f793157470af9f5c96e0fd
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-19 14:18:34 +0200

    Sync Codewars solutions - 2025-04-19 14:18:34

---
+++ javascript/5-kyu/simple_pig_latin.js
@@ -0,0 +1,11 @@
+/**
+# Simple Pig Latin
+# simple-pig-latin
+# Rank: 5 kyu
+# URL: https://www.codewars.com/kata/520b9d2ad5c005041100000f
+# Tags: Regular Expressions, Algorithms
+# Completed at: 2025-04-19
+
+*/
+
+// Solution goes here
