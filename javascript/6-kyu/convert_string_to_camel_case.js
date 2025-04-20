1b153c5cdcdafe71f0dc3643271205b582e2b34a
okrupke <156464481+okrupke@users.noreply.github.com>
2025-04-20 17:04:00 +0200

    Added code for convert_string_to_camel_case.js

--- javascript/6-kyu/convert_string_to_camel_case.js
+++ javascript/6-kyu/convert_string_to_camel_case.js
@@ -9,3 +9,20 @@
 */
 
 // Solution goes here
+// Function to convert dash/underscore-delimited string to camelCase
+function toCamelCase(str){
+  // Split the input string into words using - or _ as delimiters
+  let array = str.split(/[-_]/);
+
+  // Initialize the output with the first word (keep its original casing)
+  let output = array[0];
+
+  // Loop through the remaining words starting from the second
+  for(let i = 1; i < array.length; i++){
+    // Capitalize the first letter and append the rest of the word
+    output += array[i].charAt(0).toUpperCase() + array[i].slice(1);
+  }
+
+  // Return the camelCased string
+  return output;
+}
