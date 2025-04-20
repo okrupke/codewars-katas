79845243aeee57ed6eeeea6abfe75769654a9275
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-20 17:03:32 +0200

    Sync Codewars solutions - 2025-04-20 17:03:32

---
+++ javascript/6-kyu/convert_string_to_camel_case.js
@@ -0,0 +1,11 @@
+/**
+# Convert string to camel case
+# convert-string-to-camel-case
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/517abf86da9663f1d2000003
+# Tags: Regular Expressions, Algorithms, Strings
+# Completed at: 2025-04-20
+
+*/
+
+// Solution goes here
