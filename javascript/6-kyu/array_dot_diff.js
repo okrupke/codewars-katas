5a684d5def268e50ad6534ce46d7c2b6e861d952
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-20 18:05:18 +0200

    Sync Codewars solutions - 2025-04-20 18:05:18

---
+++ javascript/6-kyu/array_dot_diff.js
@@ -0,0 +1,11 @@
+/**
+# Array.diff
+# array-dot-diff
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/523f5d21c841566fde000009
+# Tags: Arrays, Fundamentals, Algorithms
+# Completed at: 2025-04-20
+
+*/
+
+// Solution goes here
