bb1fdbc8b4da711fa606ab62b398a83cfdd83bb3
okrupke <156464481+okrupke@users.noreply.github.com>
2025-04-20 19:04:00 +0200

    Added the code for pete_the_baker.js

--- javascript/5-kyu/pete_the_baker.js
+++ javascript/5-kyu/pete_the_baker.js
@@ -9,3 +9,29 @@
 */
 
 // Solution goes here
+// Function to calculate maximum number of cakes that can be made
+function cakes(recipe, available) {
+  
+  // Start with assuming an unlimited number of cakes
+  let numberOfCakes = Infinity;
+  
+  // Loop through each ingredient in the recipe
+  for (let ingredient in recipe) {
+    
+    // Calculate the maximum cakes that can be made with the current ingredient
+    let maxCakes = available[ingredient] / recipe[ingredient];
+    
+    // If an ingredient is missing, return 0
+    if (available[ingredient] === undefined) {
+      return 0;
+    }
+    
+    // Update the number of cakes to the minimum possible based on available ingredients
+    if (numberOfCakes > maxCakes) {
+      numberOfCakes = Math.floor(maxCakes);  // Round down to an integer
+    }
+  }
+  
+  // Return the maximum number of cakes that can be made
+  return numberOfCakes;
+}
