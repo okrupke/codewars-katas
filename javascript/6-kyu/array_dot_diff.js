0542acc52597a6306a32c2d3e6f413d8ff2e778b
okrupke <156464481+okrupke@users.noreply.github.com>
2025-04-20 18:05:49 +0200

    Added code for array_dot_diff.js

--- javascript/6-kyu/array_dot_diff.js
+++ javascript/6-kyu/array_dot_diff.js
@@ -9,3 +9,31 @@
 */
 
 // Solution goes here
+function arrayDiff(a, b) {
+  // Initialize an empty array to store the results
+  let output = [];
+  
+  // Loop through each element in the first array `a`
+  for (let i = 0; i < a.length; i++) {
+    // Set a flag to track if the current element in `a` is found in `b`
+    let found = false;
+    
+    // Loop through each element in the second array `b`
+    for (let j = 0; j < b.length; j++) {
+      // If the element in `a` matches an element in `b`
+      if (a[i] === b[j]) {
+        // Set the flag to true and break out of the inner loop
+        found = true;
+        break;
+      }
+    }
+    
+    // If the element in `a` was not found in `b`, add it to the `output` array
+    if (found === false) {
+      output.push(a[i]);
+    }
+  }
+  
+  // Return the final result, which is an array of elements from `a` not found in `b`
+  return output;
+}
