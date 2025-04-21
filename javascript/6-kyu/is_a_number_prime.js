9499cd890d70cb213ce1960df0444f57aaee9204
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-21 10:03:36 +0200

    Sync Codewars solutions - 2025-04-21 10:03:36

---
+++ javascript/6-kyu/is_a_number_prime.js
@@ -0,0 +1,11 @@
+/**
+# Is a number prime?
+# is-a-number-prime
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/5262119038c0985a5b00029f
+# Tags: Mathematics, Algorithms
+# Completed at: 2025-04-21
+
+*/
+
+// Solution goes here
