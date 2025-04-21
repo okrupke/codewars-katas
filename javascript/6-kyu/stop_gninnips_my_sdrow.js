605d0d15a6af86f1cd4715c179a830b16c61658d
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-21 12:17:29 +0200

    Sync Codewars solutions - 2025-04-21 12:17:29

---
+++ javascript/6-kyu/stop_gninnips_my_sdrow.js
@@ -0,0 +1,11 @@
+/**
+# Stop gninnipS My sdroW!
+# stop-gninnips-my-sdrow
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/5264d2b162488dc400000001
+# Tags: Strings, Algorithms
+# Completed at: 2025-04-21
+
+*/
+
+// Solution goes here
