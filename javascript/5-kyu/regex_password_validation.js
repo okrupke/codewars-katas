e86a95505765736ab86604f4690dd7fa476eaf7c
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-23 11:36:32 +0200

    Sync Codewars solutions - 2025-04-23 11:36:32

---
+++ javascript/5-kyu/regex_password_validation.js
@@ -0,0 +1,11 @@
+/**
+# Regex Password Validation
+# regex-password-validation
+# Rank: 5 kyu
+# URL: https://www.codewars.com/kata/52e1476c8147a7547a000811
+# Tags: Regular Expressions, Fundamentals
+# Completed at: 2025-04-23
+
+*/
+
+// Solution goes here
