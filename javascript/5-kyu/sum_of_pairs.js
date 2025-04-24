5955eee261ad417b79e423d6bd90547ae235fead
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-24 10:47:01 +0200

    Sync Codewars solutions - 2025-04-24 10:47:01

---
+++ javascript/5-kyu/sum_of_pairs.js
@@ -0,0 +1,11 @@
+/**
+# Sum of Pairs
+# sum-of-pairs
+# Rank: 5 kyu
+# URL: https://www.codewars.com/kata/54d81488b981293527000c8f
+# Tags: Memoization, Fundamentals, Performance
+# Completed at: 2025-04-24
+
+*/
+
+// Solution goes here
