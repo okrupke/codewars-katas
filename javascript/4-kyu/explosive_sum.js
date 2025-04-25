7a6fbc3a3a84ba21fddb72b130eb47b2ae511423
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-25 20:38:00 +0200

    Sync Codewars solutions - 2025-04-25 20:38:00

---
+++ javascript/4-kyu/explosive_sum.js
@@ -0,0 +1,11 @@
+/**
+# Explosive Sum
+# explosive-sum
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/52ec24228a515e620b0005ef
+# Tags: Algorithms, Mathematics, Fundamentals
+# Completed at: 2025-04-25
+
+*/
+
+// Solution goes here
