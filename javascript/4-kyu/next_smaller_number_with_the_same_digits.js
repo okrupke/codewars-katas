4adf8da64f2ae2121b5e256ed252eda388057536
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-26 08:13:29 +0200

    Sync Codewars solutions - 2025-04-26 08:13:29

---
+++ javascript/4-kyu/next_smaller_number_with_the_same_digits.js
@@ -0,0 +1,11 @@
+/**
+# Next smaller number with the same digits
+# next-smaller-number-with-the-same-digits
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/5659c6d896bc135c4c00021e
+# Tags: Strings, Mathematics, Algorithms
+# Completed at: 2025-04-26
+
+*/
+
+// Solution goes here
