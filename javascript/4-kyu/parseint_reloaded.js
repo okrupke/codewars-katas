fb72b2768fc0631ac00383620282bdb66f3ccb4d
Oskar <oskar@Oskars-MacBook-Air.local>
2025-04-28 14:59:30 +0200

    Sync Codewars solutions - 2025-04-28 14:59:30

---
+++ javascript/4-kyu/parseint_reloaded.js
@@ -0,0 +1,11 @@
+/**
+# parseInt() reloaded
+# parseint-reloaded
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/525c7c5ab6aecef16e0001a5
+# Tags: Parsing, Strings, Algorithms
+# Completed at: 2025-04-28
+
+*/
+
+// Solution goes here
