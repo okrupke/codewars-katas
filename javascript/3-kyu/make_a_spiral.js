e01f0edfd3ef4cfd6f5075090c730cf09b86057f
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-02 19:56:44 +0200

    Sync Codewars solutions - 2025-05-02 19:56:44

---
+++ javascript/3-kyu/make_a_spiral.js
@@ -0,0 +1,11 @@
+/**
+# Make a spiral
+# make-a-spiral
+# Rank: 3 kyu
+# URL: https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+# Tags: Algorithms, Arrays, Logic
+# Completed at: 2025-05-02
+
+*/
+
+// Solution goes here
