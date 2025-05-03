65ba19b92c5ed25103a8b749b2b28e75de612b58
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-03 11:57:02 +0200

    Sync Codewars solutions - 2025-05-03 11:57:02

---
+++ javascript/4-kyu/strip_comments.js
@@ -0,0 +1,11 @@
+/**
+# Strip Comments
+# strip-comments
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/51c8e37cee245da6b40000bd
+# Tags: Strings, Algorithms
+# Completed at: 2025-05-03
+
+*/
+
+// Solution goes here
