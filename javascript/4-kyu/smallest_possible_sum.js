e0bcb42f33924620834edb0e4dc6703953deca65
okrupke <156464481+okrupke@users.noreply.github.com>
2025-05-04 17:07:01 +0200

    Added code for smallest_possible_sum.js

--- javascript/4-kyu/smallest_possible_sum.js
+++ javascript/4-kyu/smallest_possible_sum.js
@@ -9,3 +9,24 @@
 */
 
 // Solution goes here
+// Euclid's algorithm to find the greatest common divisor (GCD) of two numbers
+function gcd(a, b) {
+  // Base case: if b is 0, then GCD is a
+  // Otherwise, recursively call gcd with (b, a mod b)
+  return b === 0 ? a : gcd(b, a % b);
+}
+
+// Function to calculate the total number of items evenly divisible by the GCD
+function solution(numbers) {
+  // Start with the first number as the initial GCD
+  let g = numbers[0];
+
+  // Compute the GCD of the entire array
+  for (let i = 1; i < numbers.length; i++) {
+    g = gcd(g, numbers[i]); // Keep updating GCD as we iterate
+  }
+
+  // Return the product of GCD and the number of elements
+  // This represents the total number of base units if we evenly divide all elements
+  return g * numbers.length;
+}
