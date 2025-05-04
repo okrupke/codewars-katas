b36e22be75712be6cd76579e9bcd00dde8f00ac7
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-04 17:06:02 +0200

    Sync Codewars solutions - 2025-05-04 17:06:02

---
+++ javascript/4-kyu/smallest_possible_sum.js
@@ -0,0 +1,11 @@
+/**
+# Smallest possible sum 
+# smallest-possible-sum
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/52f677797c461daaf7000740
+# Tags: Algorithms, Mathematics, Arrays
+# Completed at: 2025-05-04
+
+*/
+
+// Solution goes here
