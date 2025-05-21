81e7491ef7d921815842d5bbdb7fbe7c589b00d5
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-21 17:51:04 +0200

    Sync Codewars solutions - 2025-05-21 17:51:04

---
+++ javascript/3-kyu/texas_holdem_hands.js
@@ -0,0 +1,11 @@
+/**
+# Texas Hold'em Hands
+# texas-holdem-hands
+# Rank: 3 kyu
+# URL: https://www.codewars.com/kata/524c74f855025e2495000262
+# Tags: Algorithms
+# Completed at: 2025-05-21
+
+*/
+
+// Solution goes here
