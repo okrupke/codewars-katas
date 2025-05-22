22698b325a0f9ac82be51a815d72284ae3d76b10
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-22 16:17:57 +0200

    Sync Codewars solutions - 2025-05-22 16:17:57

---
+++ javascript/3-kyu/alphabetic_anagrams.js
@@ -0,0 +1,11 @@
+/**
+# Alphabetic Anagrams
+# alphabetic-anagrams
+# Rank: 3 kyu
+# URL: https://www.codewars.com/kata/53e57dada0cb0400ba000688
+# Tags: Mathematics, Logic, Strings, Algorithms
+# Completed at: 2025-05-22
+
+*/
+
+// Solution goes here
