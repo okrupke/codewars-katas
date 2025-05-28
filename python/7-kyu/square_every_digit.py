d7d3ca3a0ac355692267e1021518158630b1a6dc
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-28 18:04:38 +0200

    Sync Codewars solutions - 2025-05-28 18:04:38

---
+++ python/7-kyu/square_every_digit.py
@@ -0,0 +1,10 @@
+'''# Square Every Digit
+# square-every-digit
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/546e2562b03326a88e000020
+# Tags: Mathematics, Fundamentals
+# Completed at: 2025-05-28
+
+'''
+
+# Solution goes here
