f873b007c76b8727305bd5a7237e681e6587a1c1
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-28 04:13:38 +0200

    Sync Codewars solutions - 2025-05-28 04:13:38

---
+++ javascript/2-kyu/evaluate_mathematical_expression.js
@@ -0,0 +1,11 @@
+/**
+# Evaluate mathematical expression
+# evaluate-mathematical-expression
+# Rank: 2 kyu
+# URL: https://www.codewars.com/kata/52a78825cdfc2cfc87000005
+# Tags: Mathematics, Parsing, Algorithms
+# Completed at: 2025-05-28
+
+*/
+
+// Solution goes here
