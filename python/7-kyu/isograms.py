2fa542596dd2f8840e70b4ebb93d258b53f16381
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-29 12:14:00 +0200

    Sync Codewars solutions - 2025-05-29 12:14:00

---
+++ python/7-kyu/isograms.py
@@ -0,0 +1,10 @@
+'''# Isograms
+# isograms
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/54ba84be607a92aa900000f1
+# Tags: Strings, Fundamentals
+# Completed at: 2025-05-29
+
+'''
+
+# Solution goes here
