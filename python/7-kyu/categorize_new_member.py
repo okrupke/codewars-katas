af2b263f1d733646ea910c705cfcecf85c3f43ff
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-30 13:09:14 +0200

    Sync Codewars solutions - 2025-05-30 13:09:14

---
+++ python/7-kyu/categorize_new_member.py
@@ -0,0 +1,10 @@
+'''# Categorize New Member
+# categorize-new-member
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa
+# Tags: Fundamentals
+# Completed at: 2025-05-30
+
+'''
+
+# Solution goes here
