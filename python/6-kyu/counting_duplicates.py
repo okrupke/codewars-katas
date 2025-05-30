e06a52747c5846a720f036621b050e3a6ad8d78e
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-30 13:01:26 +0200

    Sync Codewars solutions - 2025-05-30 13:01:26

---
+++ python/6-kyu/counting_duplicates.py
@@ -0,0 +1,10 @@
+'''# Counting Duplicates
+# counting-duplicates
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1
+# Tags: Strings, Fundamentals
+# Completed at: 2025-05-30
+
+'''
+
+# Solution goes here
