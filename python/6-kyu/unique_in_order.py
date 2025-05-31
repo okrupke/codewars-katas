bcc734f579f5768564bcfbd2669a18fe499138de
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-31 18:41:06 +0200

    Sync Codewars solutions - 2025-05-31 18:41:06

---
+++ python/6-kyu/unique_in_order.py
@@ -0,0 +1,10 @@
+'''# Unique In Order
+# unique-in-order
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/54e6533c92449cc251001667
+# Tags: Algorithms, Fundamentals
+# Completed at: 2025-05-31
+
+'''
+
+# Solution goes here
