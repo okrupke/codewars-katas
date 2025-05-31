fdb0ac06051c3e5596134955501e7ff7419af268
okrupke <156464481+okrupke@users.noreply.github.com>
2025-05-31 18:41:33 +0200

    Update unique_in_order.py

--- python/6-kyu/unique_in_order.py
+++ python/6-kyu/unique_in_order.py
@@ -8,3 +8,12 @@
 '''
 
 # Solution goes here
+# Function to return a list of elements without any consecutive duplicates
+def unique_in_order(sequence):
+    output = []  # Initialize list to hold unique values in order
+    last_letter = None 
+    for letter in sequence:
+        if letter != last_letter:
+            output.append(letter) 
+            last_letter = letter  # Update last_letter only when a new letter is added
+    return output  # Return the deduplicated list
