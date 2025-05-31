6fbe95bc6079a7a14515feed0f0b001cdf630fdb
Oskar <oskar@Oskars-MacBook-Air.local>
2025-05-31 18:31:49 +0200

    Sync Codewars solutions - 2025-05-31 18:31:49

---
+++ python/7-kyu/number_of_people_in_the_bus.py
@@ -0,0 +1,10 @@
+'''# Number of People in the Bus
+# number-of-people-in-the-bus
+# Rank: 7 kyu
+# URL: https://www.codewars.com/kata/5648b12ce68d9daa6b000099
+# Tags: Fundamentals
+# Completed at: 2025-05-31
+
+'''
+
+# Solution goes here
