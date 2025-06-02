1a2329e6640f18452d7a56b8864f76b1854d326c
okrupke <156464481+okrupke@users.noreply.github.com>
2025-06-02 17:53:08 +0200

    Added code for maximum_subarray_sum.py

--- python/5-kyu/maximum_subarray_sum.py
+++ python/5-kyu/maximum_subarray_sum.py
@@ -8,3 +8,21 @@
 '''
 
 # Solution goes here
+def maxSequence(numbers):
+
+    # Initialize best_sum to 0, as the max sum of an empty subarray is 0
+    best_sum = float(0)
+
+    # Running sum of the current subarray being considered
+    current_sum = 0
+
+    # Iterate through each number in the input list
+    for x in numbers:
+        # Decide whether to add the current number to the existing subarray
+        # or start a new subarray starting at current element
+        current_sum = max(x, current_sum + x)
+
+        # Update best_sum if the current_sum is greater
+        best_sum = max(best_sum, current_sum)
+
+    return best_sum  # Return the maximum subarray sum found
