6ba10fcff8c40acb006fc9a26bf58ad7cf8de70d
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-02 17:52:18 +0200

    Sync Codewars solutions - 2025-06-02 17:52:18

---
+++ python/5-kyu/maximum_subarray_sum.py
@@ -0,0 +1,10 @@
+'''# Maximum  subarray sum
+# maximum-subarray-sum
+# Rank: 5 kyu
+# URL: https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c
+# Tags: Algorithms, Lists, Dynamic Programming, Fundamentals, Performance
+# Completed at: 2025-06-02
+
+'''
+
+# Solution goes here
