76fb668348d6fe33056b93b00845dbea4f58a03a
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-04 23:44:28 +0200

    Sync Codewars solutions - 2025-06-04 23:44:28

---
+++ python/6-kyu/are_they_the_same.py
@@ -0,0 +1,10 @@
+'''# Are they the "same"?
+# are-they-the-same
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/550498447451fbbd7600041c
+# Tags: Fundamentals
+# Completed at: 2025-06-04
+
+'''
+
+# Solution goes here
