11ec2305b99b82cdb27df8eb88235562f70ff190
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-05 20:42:29 +0200

    Sync Codewars solutions - 2025-06-05 20:42:29

---
+++ python/6-kyu/roman_numerals_encoder.py
@@ -0,0 +1,10 @@
+'''# Roman Numerals Encoder
+# roman-numerals-encoder
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/51b62bf6a9c58071c600001b
+# Tags: Algorithms
+# Completed at: 2025-06-05
+
+'''
+
+# Solution goes here
---
+++ python/6-kyu/who_likes_it.py
@@ -0,0 +1,10 @@
+'''# Who likes it?
+# who-likes-it
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+# Tags: Strings, Fundamentals
+# Completed at: 2025-06-05
+
+'''
+
+# Solution goes here
