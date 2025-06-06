99d96c650ad9e81b55f5183b03e54724ed99e090
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-06 14:54:14 +0200

    Sync Codewars solutions - 2025-06-06 14:54:14

---
+++ python/6-kyu/replace_with_alphabet_position.py
@@ -0,0 +1,10 @@
+'''# Replace With Alphabet Position
+# replace-with-alphabet-position
+# Rank: 6 kyu
+# URL: https://www.codewars.com/kata/546f922b54af40e1e90001da
+# Tags: Strings, Fundamentals
+# Completed at: 2025-06-06
+
+'''
+
+# Solution goes here
