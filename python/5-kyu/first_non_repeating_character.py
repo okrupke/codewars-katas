a6aa969f44ca7a99ac09bb99bb004a5af874244d
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-12 22:38:27 +0200

    Sync Codewars solutions - 2025-06-12 22:38:27

---
+++ python/5-kyu/first_non_repeating_character.py
@@ -0,0 +1,10 @@
+'''# First non-repeating character
+# first-non-repeating-character
+# Rank: 5 kyu
+# URL: https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+# Tags: Strings, Algorithms
+# Completed at: 2025-06-12
+
+'''
+
+# Solution goes here
