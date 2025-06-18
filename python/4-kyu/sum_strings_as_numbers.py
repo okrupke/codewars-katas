69d980667a89f6341fd68110f30bcd9b87fa1d20
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-18 18:46:17 +0200

    Sync Codewars solutions - 2025-06-18 18:46:17

---
+++ python/4-kyu/sum_strings_as_numbers.py
@@ -0,0 +1,10 @@
+'''# Sum Strings as Numbers
+# sum-strings-as-numbers
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/5324945e2ece5e1f32000370
+# Tags: Strings, Big Integers, Algorithms
+# Completed at: 2025-06-18
+
+'''
+
+# Solution goes here
