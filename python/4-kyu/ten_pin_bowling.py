a3d131e22d05f31f0d30af9c16a1634830cdb1ca
Oskar <oskar@Oskars-MacBook-Air.local>
2025-06-23 20:29:06 +0200

    Sync Codewars solutions - 2025-06-23 20:29:06

---
+++ python/4-kyu/ten_pin_bowling.py
@@ -0,0 +1,10 @@
+'''# Ten-Pin Bowling
+# ten-pin-bowling
+# Rank: 4 kyu
+# URL: https://www.codewars.com/kata/5531abe4855bcc8d1f00004c
+# Tags: Algorithms, Puzzles
+# Completed at: 2025-06-23
+
+'''
+
+# Solution goes here
