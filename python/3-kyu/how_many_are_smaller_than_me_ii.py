d906968bb486d7be9fd7e2f0c0086c8d8cc5938d
Oskar <oskar@Oskars-MacBook-Air.local>
2025-07-03 23:53:50 +0200

    Sync Codewars solutions - 2025-07-03 23:53:50

---
+++ python/3-kyu/how_many_are_smaller_than_me_ii.py
@@ -0,0 +1,10 @@
+'''# How many are smaller than me II?
+# how-many-are-smaller-than-me-ii
+# Rank: 3 kyu
+# URL: https://www.codewars.com/kata/56a1c63f3bc6827e13000006
+# Tags: Algorithms, Performance
+# Completed at: 2025-07-03
+
+'''
+
+# Solution goes here
